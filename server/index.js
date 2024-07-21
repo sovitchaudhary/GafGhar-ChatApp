40da9d3b0d9da26061c4f5801af915be4736fd01
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-07-22 04:07:29 +0530

    Create server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5001;
+const databaseURL = process.env.DATABASE_URL;
+
+const server = app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
