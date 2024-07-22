e346b41a430d8ff153faa490e463bbc66700ac21
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-07-22 16:21:33 +0530

    Add methods and cookieParser

--- server/index.js
+++ server/index.js
@@ -10,6 +10,17 @@ const app = express();
 const port = process.env.PORT || 5001;
 const databaseURL = process.env.DATABASE_URL;
 
+app.use(
+  cors({
+    origin: [process.env.ORIGIN],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
 const server = app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
