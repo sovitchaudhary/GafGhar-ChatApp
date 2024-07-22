040b8c53e9800c8bfde46780c1b7fad7eb693862
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-07-22 15:41:55 +0530

    Database connection

--- server/index.js
+++ server/index.js
@@ -13,3 +13,8 @@ const databaseURL = process.env.DATABASE_URL;
 const server = app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+mongoose
+  .connect(databaseURL)
+  .then(() => console.log("Database connection successfully!"))
+  .catch((err) => console.log(err.message));
