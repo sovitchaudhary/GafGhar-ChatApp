d3f461e8c3a879884cc3fce5ff5bce57c6bffec6
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-07-25 23:18:40 +0530

    Added components for chat

---
+++ client/src/pages/chat/components/contacts-container/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContactsContainer = () => {
+  return (
+    <div>ContactsContainer</div>
+  )
+}
+
+export default ContactsContainer
---
+++ client/src/pages/chat/components/empty-chat-container/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EmptyChatContainer = () => {
+  return (
+    <div>EmptyChatContainer</div>
+  )
+}
+
+export default EmptyChatContainer
