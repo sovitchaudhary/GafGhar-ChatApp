106d8a20ac9bf774b703b3974cf644b446858402
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-07-25 06:05:17 +0530

    Added addImage and deleteImage functionality

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -2,6 +2,7 @@ import { request, response } from "express";
 import User from "../models/UserModel.js";
 import jwt from "jsonwebtoken";
 import { compare } from "bcrypt";
+import { renameSync, unlinkSync } from "fs";
 
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 
@@ -126,6 +127,56 @@ export const updateProfile = async (request, response, next) => {
       image: userData.image,
       color: userData.color,
     });
+  } catch (error) {
+    console.log({ error });
+    return response.status(500).send("Internal Server Error");
+  }
+};
+
+//add profile image
+export const addProfileImage = async (request, response, next) => {
+  try {
+    if (!request.file) {
+      return response.status(400).send("File is required.");
+    }
+
+    const date = Date.now();
+    let fileName = "uploads/profiles/" + date + request.file.originalname;
+    renameSync(request.file.path, fileName);
+
+    const updateUser = await User.findByIdAndUpdate(
+      request.userId,
+      { image: fileName },
+      { new: true, runValidators: true }
+    );
+
+    return response.status(200).json({
+      image: updateUser.image,
+    });
+  } catch (error) {
+    console.log({ error });
+    return response.status(500).send("Internal Server Error");
+  }
+};
+
+//remove profile Image
+export const removeProfileImage = async (request, response, next) => {
+  try {
+    const { userId } = request;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return response.status(404).send("User not found.");
+    }
+
+    if (user.image) {
+      unlinkSync(user.image);
+    }
+
+    user.image = null;
+    await user.save();
+
+    return response.status(200).send("Profile image removed successfully.");
   } catch (error) {
     console.log({ error });
     return response.status(500).send("Internal Server Error");
