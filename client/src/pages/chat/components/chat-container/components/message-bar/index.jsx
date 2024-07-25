480d3382b330bb65a0fe56fb18b14abcfcc77200
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-07-26 01:39:06 +0530

    Design MessageBar component

--- client/src/pages/chat/components/chat-container/components/message-bar/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-bar/index.jsx
@@ -1,7 +1,37 @@
-const MesssageBar = () => {
-  return (
-    <div>MesssageBar</div>
-  )
-}
+import { useState } from "react";
+import { GrAttachment } from "react-icons/gr";
+import { IoSend } from "react-icons/io5";
+import { RiEmojiStickerLine } from "react-icons/ri";
 
-export default MesssageBar
+const MesssageBar = () => {
+  const [message, setMessage] = useState("");
+
+  const handleSendMessage = async () => {};
+
+  return (
+    <div className="h-[10vh] bg-[#1c1d25] flex justify-center items-center px-8 mb-6 gap-6 ">
+      <div className="flex-1 flex bg-[#2a2b33] rounded-full items-center gap-5 pr-5 ">
+        <input
+          type="text"
+          className="flex-1 p-5 bg-transparent rounded-md focus:border-none focus:outline-none "
+          placeholder="Enter Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all ">
+          <GrAttachment className="text-2xl" />
+        </button>
+        <div className="relative">
+          <button className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all ">
+            <RiEmojiStickerLine className="text-2xl" />
+          </button>
+        </div>
+      </div>
+      <button className="bg-[#8417ff] rounded-full flex items-center justify-center p-5 focus:border-none hover:bg-[#741bda] focus:bg-[#741bda] focus:outline-none focus:text-white duration-300 transition-all " onClick={handleSendMessage}>
+        <IoSend className="text-2xl" />
+      </button>
+    </div>
+  );
+};
+
+export default MesssageBar;
