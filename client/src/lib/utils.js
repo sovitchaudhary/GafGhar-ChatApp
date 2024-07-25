d22fac40466bf1334199eb87e2875e6aadf32878
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-07-26 00:13:06 +0530

    Add animationData for animation

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -1,5 +1,6 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import animationData from "@/assets/lottie-json";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
@@ -21,4 +22,10 @@ export const getColor = (color) => {
   }
 
   return colors[0]; // Fallback to the first color if out of range
+};
+
+export const animationDefaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData,
 };
