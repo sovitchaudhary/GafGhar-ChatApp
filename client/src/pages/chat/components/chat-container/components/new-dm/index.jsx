9b9998e50ab48ac64c51d2c194d8d6ccf36377ee
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-07-28 01:17:18 +0530

    Design new-dm component

--- client/src/pages/chat/components/chat-container/components/new-dm/index.jsx
+++ client/src/pages/chat/components/chat-container/components/new-dm/index.jsx
@@ -1,7 +1,72 @@
-const NewDM = () => {
-  return (
-    <div>NewDM</div>
-  )
-}
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 
-export default NewDM
+import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import { Input } from "@/components/ui/input";
+import { animationDefaultOptions } from "@/lib/utils";
+import Lottie from "react-lottie";
+
+const NewDM = () => {
+  const [openNewContactModal, setOpenNewContactModal] = useState(false);
+  const [searchedContacts, setsetSearchedContacts] = useState([]);
+
+  const searchContacts = async (searchTerm) => {};
+
+  return (
+    <>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <FaPlus
+              className="text-neutral-400 font-light text-opacity-90 text-start hover:text-neutral-100 cursor-pointer transition-all duration-300"
+              onClick={() => setOpenNewContactModal(true)}
+            />
+          </TooltipTrigger>
+          <TooltipContent className="bg-[#1c1b1e] border-none mb-2 p-3 text-white ">
+            Select New Contact
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <Dialog open={openNewContactModal} onOpenChange={setOpenNewContactModal}>
+        <DialogContent className="bg-[#1c1d25] border-none text-white w-[400px] h-[400px] flex flex-col ">
+          <DialogHeader>
+            <DialogTitle>Please select a contact</DialogTitle>
+            <DialogDescription></DialogDescription>
+          </DialogHeader>
+          <div>
+            <Input
+              placeholder="Search Contacts"
+              className="rounded-lg p-6 bg-[#2c2e3b] border-none"
+              onClick={(e) => searchContacts(e.target.value)}
+            />
+          </div>
+          {searchedContacts.length <= 0 && (
+            <div className="flex-1 md:bg-[#1c1d25] md:flex flex-col justify-center mt-5 items-center duration-1000 transition-all">
+              <Lottie
+                isClickToPauseDisabled={true}
+                height={100}
+                width={100}
+                options={animationDefaultOptions}
+              />
+              <div className="text-opacity-80 text-white flex flex-col gap-5 items-center mt-5 lg:text-4xl text-xl transition-all duration-300 text-center ">
+                <h3 className="poppins-medium">
+                  Hi<span className="text-purple-500">!</span> Search New
+                  <span className="text-purple-500"> Contact. </span>
+                </h3>
+              </div>
+            </div>
+          )}
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default NewDM;
