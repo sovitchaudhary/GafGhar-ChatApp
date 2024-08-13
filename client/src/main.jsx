e288b4af3148cf6cc50c9514a08d8e4e240fb598
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-08-14 00:14:15 +0530

    Set up Socket.io on the backend for real-time communication

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Toaster } from "./components/ui/sonner.jsx";
+import { SocketProvider } from "./context/SocketContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   //<React.StrictMode>
   <>
-    <App />
-    <Toaster closeButton />
+    <SocketProvider>
+      <App />
+      <Toaster closeButton />
+    </SocketProvider>
   </>
   //</React.StrictMode>,
 );
