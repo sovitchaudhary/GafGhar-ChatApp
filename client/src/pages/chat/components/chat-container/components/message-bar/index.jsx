65c10143133adb4254d9a76f02825901028e261b
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-08-15 04:55:59 +0530

    Added Send and Receive Files functionality

--- client/src/pages/chat/components/chat-container/components/message-bar/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-bar/index.jsx
@@ -1,5 +1,7 @@
 import { useSocket } from "@/context/SocketContext";
+import { apiClient } from "@/lib/api-client";
 import { useAppStore } from "@/store";
+import { UPLOAD_FILE_ROUTE } from "@/utils/constants";
 import EmojiPicker from "emoji-picker-react";
 import { useEffect, useRef, useState } from "react";
 import { GrAttachment } from "react-icons/gr";
@@ -8,8 +10,9 @@ import { RiEmojiStickerLine } from "react-icons/ri";
 
 const MesssageBar = () => {
   const emojiRef = useRef();
+  const fileInputRef = useRef();
   const socket = useSocket();
-  const { selectedChatType, selectedChatData,userInfo } = useAppStore();
+  const { selectedChatType, selectedChatData, userInfo } = useAppStore();
   const [message, setMessage] = useState("");
   const [emojiPickerOpen, setEmojiPickerOpen] = useState(false);
 
@@ -41,6 +44,40 @@ const MesssageBar = () => {
     }
   };
 
+  const handleAttachmentClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleAttachmentChange = async () => {
+    try {
+      const file = event.target.files[0];
+      if (file) {
+        const formData = new FormData();
+        formData.append("file", file);
+        const response = await apiClient.post(UPLOAD_FILE_ROUTE, formData, {
+          withCredentials: true,
+        });
+
+        if (response.status === 200 && response.data) {
+          if (selectedChatType === "contact") {
+            socket.emit("sendMessage", {
+              sender: userInfo.id,
+              content: undefined,
+              recipient: selectedChatData._id,
+              messageType: "file",
+              fileUrl: response.data.filePath,
+            });
+          }
+        }
+      }
+      console.log({ file });
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
   return (
     <div className="h-[10vh] bg-[#1c1d25] flex justify-center items-center px-8 mb-6 gap-6 ">
       <div className="flex-1 flex bg-[#2a2b33] rounded-full items-center gap-5 pr-5 ">
@@ -51,9 +88,18 @@ const MesssageBar = () => {
           value={message}
           onChange={(e) => setMessage(e.target.value)}
         />
-        <button className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all ">
+        <button
+          className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all"
+          onClick={handleAttachmentClick}
+        >
           <GrAttachment className="text-2xl" />
         </button>
+        <input
+          type="file"
+          className="hidden "
+          ref={fileInputRef}
+          onChange={handleAttachmentChange}
+        />
         <div className="relative">
           <button
             className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all "
--- client/src/pages/chat/components/chat-container/components/message-container/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -1,8 +1,10 @@
 import { apiClient } from "@/lib/api-client";
 import { useAppStore } from "@/store";
-import { GET_ALL_MESSAGES_ROUTE } from "@/utils/constants";
+import { GET_ALL_MESSAGES_ROUTE, HOST } from "@/utils/constants";
 import moment from "moment";
 import { useEffect, useRef } from "react";
+import { MdFolderZip } from "react-icons/md";
+import { IoMdArrowRoundDown } from "react-icons/io";
 
 const MessageContainer = () => {
   const scrollRef = useRef();
@@ -40,6 +42,12 @@ const MessageContainer = () => {
     }
   }, [selectedChatMessages]);
 
+  const checkIfImage = (filePath) => {
+    const imageRegex =
+      /\.(jpg|jpeg|png|gif|bmp|tiff|tif|webp|svg|ico|heic|heif)$/i;
+    return imageRegex.test(filePath);
+  };
+
   const renderMessages = () => {
     let lastDate = null;
 
@@ -60,8 +68,16 @@ const MessageContainer = () => {
     });
   };
 
+  const downloadFile = () => {};
+
+  //render part
+
   const renderDMMessages = (message) => (
-    <div className={`${message.sender === selectedChatData._id ? "text-left" : "text-right"}`}>
+    <div
+      className={`${
+        message.sender === selectedChatData._id ? "text-left" : "text-right"
+      }`}
+    >
       {message.messageType === "text" && (
         <div
           className={`${
@@ -73,7 +89,38 @@ const MessageContainer = () => {
           {message.content}
         </div>
       )}
-      <div className="text-xs text-gray-600">{moment(message.timestamp).format("LT")}</div>
+      {message.messageType === "file" && (
+        <div
+          className={`${
+            message.sender !== selectedChatData._id
+              ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#8417ff]/50"
+              : "bg-[#2a2b33]/5 text-white/80 border-[#ffff]/50 "
+          } border inline-block p-4 rounded my-1 max-w-[50%] break-words`}
+        >
+          {checkIfImage(message.fileUrl) ? (
+            <div className="cursor-pointer">
+              <img src={`${HOST}/${message.fileUrl}`} />
+            </div>
+          ) : (
+            <div className="flex items-center justify-center gap4 ">
+              <span className="text-white/8 text-3xl bg-black/20 rounded-full p-3">
+                <MdFolderZip />
+              </span>
+              <span>{message.fileUrl.split("/").pop()}</span>
+              <span
+                className="bg-black/20 p-3 text-2xl rounded-full hover:bg-black/50 cursor-pointer translate-all duration-300"
+                onClick={() => downloadFile(message.fileUrl)}
+              >
+                {" "}
+                <IoMdArrowRoundDown />{" "}
+              </span>
+            </div>
+          )}
+        </div>
+      )}
+      <div className="text-xs text-gray-600">
+        {moment(message.timestamp).format("LT")}
+      </div>
     </div>
   );
 
