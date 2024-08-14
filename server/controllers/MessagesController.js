68ca70f967aba9c5bb1c0adecb1b21882f3c768a
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-08-15 04:00:22 +0530

    Set up the MessagesRoutes and MessagesController

--- server/controllers/MessagesController.js
+++ server/controllers/MessagesController.js
@@ -1,4 +1,5 @@
 import Message from "../models/MessagesModel.js";
+import { mkdirSync, renameSync } from "fs";
 
 export const getMessages = async (request, response, next) => {
   try {
@@ -16,6 +17,27 @@ export const getMessages = async (request, response, next) => {
       ],
     }).sort({ timestamp: 1 });
     return response.status(200).json({ messages });
+  } catch (error) {
+    console.log({ error });
+    return response.status(500).send("Internal Server Error");
+  }
+};
+
+//uploads files
+export const uploadFile = async (request, response, next) => {
+  try {
+    if (!request.file) {
+      return response.status(400).send("File is required");
+    }
+    const date = Date.now();
+    let fileDir = `uploads/files/${date}`;
+    let fileName = `${fileDir}/${request.file.originalname}`;
+
+    mkdirSync(fileDir, { recursive: true });
+
+    renameSync(request.file.path, fileName);
+
+    return response.status(200).json({ filePath: fileName });
   } catch (error) {
     console.log({ error });
     return response.status(500).send("Internal Server Error");
