baad5db363f99c7bddf5836b3c6231faca8295a3
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-08-16 21:31:19 +0530

    Added getUserChannels functionality

--- client/src/pages/chat/components/contacts-container/index.jsx
+++ client/src/pages/chat/components/contacts-container/index.jsx
@@ -2,13 +2,13 @@ import React, { useEffect } from "react";
 import ProfileInfo from "./components/profile-info";
 import NewDM from "./components/new-dm";
 import { apiClient } from "@/lib/api-client";
-import { GET_DM_CONTACTS_ROUTES } from "@/utils/constants";
+import { GET_DM_CONTACTS_ROUTES, GET_USER_CHANNELS_ROUTE } from "@/utils/constants";
 import { useAppStore } from "@/store";
 import ContactList from "@/components/contact-list";
 import CreateChannel from "./components/create-channel";
 
 const ContactsContainer = () => {
-  const { setDirectMessagesContacts, directMessagesContacts, channels } =
+  const { setDirectMessagesContacts, directMessagesContacts, channels, setChannels } =
     useAppStore();
 
   useEffect(() => {
@@ -20,8 +20,19 @@ const ContactsContainer = () => {
         setDirectMessagesContacts(response.data.contacts);
       }
     };
+    
+    const getChannels = async () => {
+      const response = await apiClient.get(GET_USER_CHANNELS_ROUTE, {
+        withCredentials: true,
+      });
+      if (response.data.channels) {
+        setChannels(response.data.channels);
+      }
+    };
+
     getContacts();
-  }, []);
+    getChannels();
+  }, [setChannels, setDirectMessagesContacts]);
 
   return (
     <div className="relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full">
--- server/controllers/ChannelController.js
+++ server/controllers/ChannelController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Channel from "../models/ChannelModel.js";
 import User from "../models/UserModel.js";
 
@@ -26,6 +27,20 @@ export const createChannel = async (request, response, next) => {
 
     await newChannel.save();
     return response.status(201).json({ channel: newChannel });
+  } catch (error) {
+    console.log({ error });
+    return response.status(500).send("Internal Server Error");
+  }
+};
+
+export const getUserChannels = async (request, response, next) => {
+  try {
+    const userId = new mongoose.Types.ObjectId(request.userId);
+    const channels = await Channel.find({
+      $or: [{ admin: userId }, { members: userId }],
+    }).sort({ updatedAt: -1 });
+
+    return response.status(201).json({ channels });
   } catch (error) {
     console.log({ error });
     return response.status(500).send("Internal Server Error");
